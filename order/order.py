7a29ea5bb7a5201503504a161254cd4c4f40bd8f
Mikesky109 <67830096+Mikesky109@users.noreply.github.com>
2021-10-25 02:11:19 +0800

    Add features for order.py

--- order/order.py
+++ order/order.py
@@ -11,7 +11,7 @@ client = MongoClient(host=os.environ['MONGO_SERVER_HOST'], port=int(port_number)
 #switch to db fakeUberEat
 db = client.fakeUberEat
 """
-def uber_eat_db():
+def connect_order_db():
     # host = os.environ['MONGO_SERVER_HOST']
     # usr = os.environ['MONGO_USERNAME']
     # pwd = os.environ['MONGO_PASSWORD']
@@ -34,12 +34,14 @@ def uber_eat_db():
     # return db after we connected server.
     db = client["fakeUberEat"]
     return db
+db = connect_order_db()
 
-client = MongoClient(host="db_order",
-                            port=27018, 
-                            username="order", 
-                            password="12345",
-                            serverSelectionTimeoutMS=1000)
+def output(result):
+    if not bool(result):
+        errormsg = {'error': 'not found'}
+        return jsonify(errormsg), 404
+    else:
+        return jsonify(result)
 
 @app.route('/')
 def todo():
@@ -55,23 +57,78 @@ def group():
     return jsonify({"group_number": "5", "group_members":[{"Name": "Wong Pan Chi", "Student_ID": "18063466D"},{"Name": "Wong Kin Sang", "Student_ID": "18051915D"},{"Name": "Yuen Chun Ming", "Student_ID": "18059819D"},{"Name": "Sin Kwo Yin", "Student_ID": "18059048D"}]}), 200
 
 #/orders: Return a JSON object with all the orders’ attributes
-@app.route ('/orders', methods=['GET'])
-def order():
+@app.route ('/orders/<orderID>/list', methods=['GET'])
+def each_order(orderID):
     #The orders sorted in ascending order of order ID.
-    db = uber_eat_db()
-    orders =db.order.find({},{"_id" : 0}).sort("store_id", 1)
+    db = connect_order_db()
+    orders =db['order'].find({"order_id": orderID},{"_id" : 0})
     result = []
     for order in orders:
         temp = {}
-        for key, value in order.items():
-            if (type(order[key])==str):
-                temp[key] = str(order[key])
-            else:
+        for key in order.keys():
+            if (type(order[key]) != str):
                 temp[key] = order[key]
-        result.append(temp)
-    return jsonify(result),200
+            else:
+                temp[key] = str(order[key])
 
+        result.append(temp)
+
+    return output(result)
+
+@app.route ('/orders', methods=['GET'])
+def order():
+    #The orders sorted in ascending order of order ID.
+    db = connect_order_db()
+    orders =db['order'].find({},{"_id" : 0}).sort("order_id", 1)
+    result = []
+    for order in orders:
+        temp = {}
+        for key in order.keys():
+            if (type(order[key]) != str):
+                temp[key] = order[key]
+            else:
+                temp[key] = str(order[key])
+
+        result.append(temp)
+
+    return output(result)
+
+@app.route ('/orders/shoplist', methods=['GET'])
+def shop_list():
+    db = connect_order_db()
+    orders =db['store_list'].find({},{"_id" : 0})
+    result = []
+    for order in orders:
+        temp = {}
+        for key in order.keys():
+            if (type(order[key]) != str):
+                temp[key] = order[key]
+            else:
+                temp[key] = str(order[key])
+
+        result.append(temp)
+
+    return output(result)
+
+@app.route ('/orders/<storeID>/addorder')
+def add_order(storeID):
+    db = connect_order_db()
+    order_id = "00001"
+    while (db["order"].count_documents({"order_id": order_id}) > 0):
+        order_id = '{:05d}'.format(int(order_id) + 1)
+        #print("order id = ", order_id)
+
+    if (db["store_list"].count_documents({"store_id": storeID}) == 1):
+        store = db['store_list'].find_one({"store_id": storeID})
+        store_name = str(store['store_name'])
+
+        
+        db['order'].insert_one({'order_id': order_id, 'store_id': storeID, 'store_name': store_name, 'customer_id': '11111'})
+        return jsonify({'order_id':order_id, 'stage':"success"})
+    else:
+        return jsonify(message="Please check Store ID")
 
 if __name__ == "__main__":
     #this Python flask REST API listen at port 15001 at 0.0.0.0 within the container.
+    client = connect_order_db()
     app.run(host='0.0.0.0', port=15001, debug=True)
