4853392cf39727c3a667724596719875b1306567
samwong0127 <74463010+samwong0127@users.noreply.github.com>
2021-10-25 16:59:39 +0800

    Update store.py
    add /api, /stores/<categories>, /stores/<store_id>

--- store/store.py
+++ store/store.py
@@ -24,6 +24,30 @@ def todo():
 def group():
     return jsonify({"group_number": "5", "group_members":[{"Name": "Wong Pan Chi", "Student_ID": "18063466D"},{"Name": "Wong Kin Sang", "Student_ID": "18051915D"},{"Name": "Yuen Chun Ming", "Student_ID": "18059819D"},{"Name": "Sin Kwo Yin", "Student_ID": "18059048D"}]}), 200
 
+# Return the API using
+@app.route ('/api', methods=['GET'])
+def whereami():
+    return jsonify({"API":"store"}), 200
+
+# Return a JSON object with all attributes of a specific store                    
+@app.route ('/stores/<store_id>', methods=['GET'])
+def get_store_with_id(store_id):
+    stores = list(db.store.find({"store_id":store_id}.{"_id":0}))
+    if len(stores) == 0:
+        return jsonify({"Error":"Store not found"}), 404
+    else:
+        return jsonify((stores)), 200
+
+# Return a JSON object with all attributes of a store with a specific category                    
+@app.route ('/stores/<categories>', methods=['GET'])
+def get_store_with_category(categories):
+    stores = list(db.store.find({"categories":categories}.{"_id":0}))
+    if len(stores) == 0:
+        return jsonify({"Error":"No such category"}), 404
+    else:
+        return jsonify(stores), 200
+
+
 
 if __name__ == "__main__":
     #this Python flask REST API listen at port 15002 at 0.0.0.0 within the container.
