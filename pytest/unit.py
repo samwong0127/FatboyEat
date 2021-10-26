97fe43cc9dd3c474cdb586226869f28e69a686cd
samwong0127 <74463010+samwong0127@users.noreply.github.com>
2021-10-26 19:21:47 +0800

    Add pytest for UI Testing

---
+++ pytest/unit.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def url_order():
+    return "http://localhost:20080/order"
+
+def url_store():
+    return "http://localhost:20080/store"
+
+def url_menu():
+    return "http://localhost:20080/menu"
+
