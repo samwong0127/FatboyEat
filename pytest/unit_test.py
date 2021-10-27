76d983e53882f607ec9abd72d35960edf8a44e98
samwong0127 <74463010+samwong0127@users.noreply.github.com>
2021-10-28 00:28:30 +0800

    Update unit_test.py

--- pytest/unit_test.py
+++ pytest/unit_test.py
@@ -1,39 +1,15 @@
 import pytest
-from unit import *
 import requests
-from flask import Flask, jsonify, request
-from pymongo import MongoClient
 import json
+from menu_test.menu_test import *
+from store_test.store_test import *
+from order_test.order_test import *
 
-def test_add():
-    assert add(3,4)==7
+"""
+* pytest this file will test everything
+*
+"""
 
-def test_sub():
-    assert sub(3,4)==-1
 
-def test_order_api():
-    response = requests.get(url_order()+"/api")
-    j = json.loads(response.text)
-    assert response.status_code == 200
-    #print(j['API'])
-    assert j['API'] == "order"
 
-def test_order_returningAttribute():
-    url = url_order()+"/orders/00001/list"
-    response = requests.get(url)
-    j = json.loads(response.text)
-    assert response.status_code == 200
-    assert j[0]['order_id'] == "00001"
 
-'''
-def test_order_addOrder():
-    store_id = '00001'
-    #order_id = "00001"
-    url = url_order()+'/orders/'+store_id+'/addorder'
-    response = requests.get(url)
-    j = json.loads(response.text)
-    # {'order_id':order_id, 'stage':"success"}
-    assert j['stage'] == 'success'
-    assert j['order_id'] == '00001'
-'''
-    
