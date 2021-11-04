961e85080618fe0ab0c878ef10571539a4d7128a
18059819d <18059819d@connect.polyu.hk>
2021-11-04 22:40:32 +0800

    Update init database for menu and order

--- menu/mongo-menu-init.js
+++ menu/mongo-menu-init.js
@@ -3,9 +3,9 @@ db = db.getSiblingDB('FatboyEat')
 
 db.createCollection('menu');
 
-db.menu.insertOne({'store_id':'00001', 'store_name':'Sushiro HK', 'dishes_id':'1', 'dishes_name': 'Sushi A', 'price': '10'});
-db.menu.insertOne({'store_id':'00001', 'store_name':'Sushiro HK', 'dishes_id':'2', 'dishes_name': 'Sushi B', 'price': '20'});
-db.menu.insertOne({'store_id':'00001', 'store_name':'Sushiro HK', 'dishes_id':'3', 'dishes_name': 'Sushi C', 'price': '30'});
+db.menu.insertOne({'store_id':'00001', 'store_name':'Sushiro HK', 'dishes_id':'00001', 'dishes_name':'Sushi A', 'price':10, 'avaliability':true});
+db.menu.insertOne({'store_id':'00001', 'store_name':'Sushiro HK', 'dishes_id':'00002', 'dishes_name':'Sushi B', 'price':20, 'avaliability':false});
+db.menu.insertOne({'store_id':'00001', 'store_name':'Sushiro HK', 'dishes_id':'00003', 'dishes_name':'Sushi C', 'price':30, 'avaliability':true});
 
-db.menu.insertOne({'store_id':'00002', 'store_name':'TamJai Yunnan Mixian', 'dishes_id':'4','dishes_name': 'Mixian B', 'price': '35'});
-db.menu.insertOne({'store_id':'00003', 'store_name':'Pizza Hut', 'dishes_id':'5','dishes_name': 'Pizza C', 'price': '128'});
+db.menu.insertOne({'store_id':'00002', 'store_name':'TamJai Yunnan Mixian', 'dishes_id':'00004','dishes_name':'Mixian B', 'price':35, 'avaliability':true});
+db.menu.insertOne({'store_id':'00003', 'store_name':'Pizza Hut', 'dishes_id':'00005','dishes_name':'Pizza C', 'price':128, 'avaliability':true});
